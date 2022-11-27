2ddaa82909ff70c098e30331a222a43c822a4451
André Lopes <75704128+andre-lopss@users.noreply.github.com>
2022-11-27 01:22:09 -0300

    Corrigindo o exercício
    Uso de lista e não de 'vetor'

--- ExerciciosPOO/ExercisesPOO2/ExercisesPOO2/Livro.cs
+++ ExerciciosPOO/ExercisesPOO2/ExercisesPOO2/Livro.cs
@@ -1,24 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExercisesPOO2
 {
     class Livro
     {
-        private String NomeLivro { get; set; }
-        private Autor[] autoresLivro;
+        public String NomeLivro { get; set; }
+        public List<Autor> Autores = new List<Autor>();
 
-        public Livro(string nomeLivro, Autor[] autor)
+        public Livro(string nomeLivro, List<Autor> autores)
         {
             NomeLivro = nomeLivro;
-            autoresLivro = autor;
+            this.Autores = autores;
         }
-  
+
         public override string ToString()
         {
             Console.Write("Nome do Livro: ");
             Console.WriteLine(NomeLivro);
             Console.WriteLine("Autores: ");
-            foreach(Autor autor in autoresLivro){ 
+            foreach(Autor autor in Autores){ 
                 Console.WriteLine(autor);
             }
             return " ";             
--- ExerciciosPOO/ExercisesPOO2/ExercisesPOO2/Program.cs
+++ ExerciciosPOO/ExercisesPOO2/ExercisesPOO2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExercisesPOO2
 {
@@ -6,12 +7,20 @@ namespace ExercisesPOO2
     {
         static void Main(string[] args)
         {
-            Livro livro1 = new Livro("As Crônicas de Nárnia", new Autor[] { new Autor("C. S. Lewis", "cslewis@hotmail.com")});
+            List<Autor> autorList1 = new List<Autor> 
+            { new Autor("C. S. Lewis", "cslewis@hotmail.com") };
 
-            Livro livro2 = new Livro("E se fosse a gente?", new Autor[] { new Autor("Adam Silveira", "adam@hotmail.com"), new Autor( "Becky Albertalli", "becky@hotmail.com")});
+            Livro livro1 = new Livro("As Crônicas de Nárnia", autorList1);
+
+            List<Autor> autorList2 = new List<Autor>
+            {
+                new Autor("Adam Silveira", "adam@hotmail.com"),
+                new Autor("Becky Albertalli", "becky@hotmail.com")
+            };
+
+            Livro livro2 = new Livro("E se fosse a gente?", autorList2);
 
             Console.WriteLine(livro1);
-
             Console.WriteLine(livro2);
 
             Console.ReadLine();
